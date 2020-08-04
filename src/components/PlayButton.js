d187bcb91a73f3dc216464e889a8c19d01f44987
yinonhever <yinonehever@gmail.com>
2020-08-04 21:50:34 +0300

    improving code

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import rock from "../images/icon-rock.svg";
 import paper from "../images/icon-paper.svg";
 import scissors from "../images/icon-scissors.svg";
@@ -6,23 +6,18 @@ import lizard from "../images/icon-lizard.svg";
 import spock from "../images/icon-spock.svg";
 
 const PlayButton = props => {
-    const [highlighted, setHighlighted] = useState(false);
-
-    let icon = "";
-    if (props.type === "rock") { icon = rock; }
-    if (props.type === "paper") { icon = paper; }
-    if (props.type === "scissors") { icon = scissors; }
-    if (props.type === "lizard") { icon = lizard; }
-    if (props.type === "spock") { icon = spock; }
+    const icons = {
+        rock: rock,
+        paper: paper,
+        scissors: scissors,
+        lizard: lizard,
+        spock: spock
+    }
 
     let classes = ["play-button", props.type];
     if (props.big) {
         classes = [...classes, "big"];
     }
-
-    useEffect(() => {
-        setHighlighted(props.highlighted);
-    }, [props.highlighted])
 
     const clickHandler = () => {
         if (props.clicked) {
@@ -35,9 +30,9 @@ const PlayButton = props => {
             cursor: props.clicked ? "pointer" : null
         }}>
             <div className="play-button__inner">
-                <img className="play-button__icon" src={icon} alt={props.type} />
+                <img className="play-button__icon" src={icons[props.type]} alt={props.type} />
             </div>
-            {highlighted ? <div className="play-button__backdrop" /> : null}
+            {props.highlighted ? <div className="play-button__backdrop" /> : null}
         </div>
     )
 }
